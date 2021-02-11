bb06f98881145dc9fc1cc3606773bf179cc16197
demidraksin <greatraksin@icloud.com>
2021-02-11 14:19:56 +0300

    Finishing code🌳

--- EXCEL to SQLITE/excel_to_db.py
+++ EXCEL to SQLITE/excel_to_db.py
@@ -4,5 +4,12 @@ import pandas as pd
 con = sqlite3.connect('my_music.db')
 # подключаемся к БД my_music
 wb = pd.read_excel('music.xlsx', sheet_name=None)
+# читаем excel файл, не указывая названия листов в нем
 
+for sheet in wb:
+    """просматриваем каждый лист внутри таблицы"""
+    wb[sheet].to_sql(sheet, con, index=False)
+
+con.commit()  # применить изменения
+con.close()  # закрываю подключение к БД
 
